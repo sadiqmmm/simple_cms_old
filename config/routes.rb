546f9b142009e7b7b7eb2319a821bfe0cf187c7d
mohammed sadiq <sadiqmmm@gmail.com>
2014-01-18 01:39:45 +0530

    Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 SimpleCms::Application.routes.draw do
-  get "demo/index"
+  root to: "demo#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
