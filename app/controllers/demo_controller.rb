f2c7926806742228e8ddad5ba68ff648a2a21210
mohammed sadiq <sadiqmmm@gmail.com>
2014-01-18 16:24:08 +0530

    demo controller with render and redirects

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,4 +1,17 @@
 class DemoController < ApplicationController
   def index
+  	render "hello"
+  end
+ 
+  def hello
+  	render "index"
+  end
+
+  def other_hello
+  	redirect_to action: 'index'
+  end
+
+  def azadlabs
+  	redirect_to ("http://azadlabs.com")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 SimpleCms::Application.routes.draw do
+  
+  get "demo/hello"
+  get "demo/index"
+  get "demo/other_hello"
+
   root to: "demo#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
